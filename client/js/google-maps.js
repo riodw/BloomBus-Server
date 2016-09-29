5ce94dd075a1152fe2fdcaef300188f818757274
paniel <paniel25@gmail.com>
2016-09-29 14:43:04 +0000

    Fix Google Maps API

--- client/js/google-maps.js
+++ client/js/google-maps.js
@@ -1,25 +1,27 @@
-/* google maps -----------------------------------------------------*/
-google.maps.event.addDomListener(window, 'load', initialize);
+/**
+ * GOOGLE MAPS
+ * initMap() - happens on callback of load "https://maps.googleapis.com/maps/api/js&callback=initMap"
+ */
+ 
+function initMap() {
 
-function initialize() {
+   /* position Amsterdam */
+   var latlng = new google.maps.LatLng(52.3731, 4.8922);
 
-  /* position Amsterdam */
-  var latlng = new google.maps.LatLng(52.3731, 4.8922);
+   var mapOptions = {
+      center: latlng,
+      scrollWheel: false,
+      zoom: 13
+   };
 
-  var mapOptions = {
-    center: latlng,
-    scrollWheel: false,
-    zoom: 13
-  };
-  
-  var marker = new google.maps.Marker({
-    position: latlng,
-    url: '/',
-    animation: google.maps.Animation.DROP
-  });
-  
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-  marker.setMap(map);
+   var marker = new google.maps.Marker({
+      position: latlng,
+      url: '/',
+      animation: google.maps.Animation.DROP
+   });
 
-};
-/* end google maps -----------------------------------------------------*/
+   var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+   marker.setMap(map);
+
+}
+/* ./END - google maps*/
