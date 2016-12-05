514e03b9f313aecb1f57c2d1a694b16affb142a0
paniel <paniel25@gmail.com>
2016-12-05 23:24:44 +0000

    Add code to get IP address of server

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ var express = require('express');
 var http = require('http');
 // Native NodeJS module for resolving paths
 var path = require('path');
-// var views_path = 
+// Used to get IP Address
+var os = require('os');
 
 var port = process.env.PORT;
 var server_IP = process.env.IP;
+
+
+
 
 
 
@@ -15,8 +19,36 @@ var pro = false;
 process.argv.forEach(function (val, index, array) {
    if(val == 'real') {
       port = 8080;
-      server_IP = "148.137.138.107";
       pro = true;
+      
+      // Set IP Address
+      var ifaces = os.networkInterfaces();
+      Object.keys(ifaces).forEach(function(ifname) {
+         var alias = 0;
+         
+         ifaces[ifname].forEach(function(iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+               // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+               return;
+            }
+            
+            if (alias >= 1) {
+               // this single interface has multiple ipv4 addresses
+               console.log(ifname + ':' + alias, iface.address);
+               throw "Error: "
+                  + "There are multiple IPV4 addresses. You must hard code the the IP address.";
+            }
+            else {
+               // this interface has only one ipv4 adress
+               console.log(ifname, iface.address);
+               // Setting IP
+               server_IP = iface.address;
+            }
+            ++alias;
+         });
+      });
+      // FOR USE WHEN IP MUST BE HARD CODED
+      // server_IP = "148.137.138.107";
    }
 });
 
@@ -56,8 +88,6 @@ var app = express();
 // Set up
 
 app.use(express.static(path.resolve(__dirname + '/client')));
-
-console.log(app.set);
 
 // Adding
 // app.use(cookieParser());
@@ -104,3 +134,8 @@ app.get('/*', function(req, res) {
 http.createServer(app).listen(port, function() {
    console.log('SERVER RUNNING... PORT: ' + port);
 });
+
+
+
+// -----------------------
+
